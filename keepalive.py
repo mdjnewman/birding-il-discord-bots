e27d78ba3381b3b6bacbbb3ab38ebfee4ebf3d07
Matt Newman <mdjnewman@gmail.com>
2024-03-21 12:59:38 -0500

    IC
    From Oliver/Matt C.

---
+++ keepalive.py
@@ -0,0 +1,12 @@
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "I'm alive"
+
+def run():
+  app.run(host='0.0.0.0',port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
