318129262c237766c92c47994d60e1120da40485
Matt Newman <mdjnewman@gmail.com>
2024-07-28 08:22:30 -0500

    Move tf version to asdf

--- deployment/providers.tf
+++
@@ -1,7 +0,0 @@
-provider "google" {
-  project = "birding-il"
-  region  = "us-central1"
-}
-
-data "google_client_config" "current" {
-}
--- deployment/terraform.tf
+++ deployment/terraform.tf
@@ -10,3 +10,11 @@ terraform {
     }
   }
 }
+
+provider "google" {
+  project = "birding-il"
+  region  = "us-central1"
+}
+
+data "google_client_config" "current" {
+}
